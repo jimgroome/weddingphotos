7ce7fa7fe5d33bd4cda716479631fad511e40dcf
Jim Groome <jim.groome@marketingdelivery.com>
2019-06-26 11:22:11 +0100

    Keyboard navigation working

--- src/App.js
+++ src/App.js
@@ -8,6 +8,32 @@ const App = () => {
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const imagesPerPage = 30;
+  const totalPages = Math.ceil(images.length / imagesPerPage);
+
+  const paginate = (e, pageNumber) => {
+    e.preventDefault();
+    setCurrentPage(pageNumber);
+  };
+
+  const logKey = e => {
+    const key = e.key;
+    let destinationPage = null;
+
+    if (key === "ArrowLeft") {
+      if (currentPage !== 1) {
+        destinationPage = currentPage - 1;
+        paginate(e, destinationPage);
+      }
+    }
+    if (key === "ArrowRight") {
+      if (currentPage !== totalPages) {
+        destinationPage = currentPage + 1;
+        paginate(e, destinationPage);
+      }
+    }
+  };
+
+  document.addEventListener("keydown", logKey);
 
   useEffect(() => {
     const fetchImages = () => {
@@ -23,11 +49,6 @@ const App = () => {
   const indexOfLastImage = currentPage * imagesPerPage;
   const indexOfFirstImage = indexOfLastImage - imagesPerPage;
   const currentImages = images.slice(indexOfFirstImage, indexOfLastImage);
-
-  const paginate = (e, pageNumber) => {
-    e.preventDefault();
-    setCurrentPage(pageNumber);
-  };
 
   return (
     <div>
