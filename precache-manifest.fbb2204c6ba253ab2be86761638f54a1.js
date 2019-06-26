b810d6a0649b93525276ee09382b6e11bff87319
Jim Groome <jim.groome@marketingdelivery.com>
2019-06-26 09:32:33 +0100

    Updates

---
+++ precache-manifest.fbb2204c6ba253ab2be86761638f54a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eafbece52a29d18e9c437f4ea56c5d8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc382cdcc27b4173f7f0",
+    "url": "/static/css/main.9c688991.chunk.css"
+  },
+  {
+    "revision": "13f932aa6f03fa87c063",
+    "url": "/static/js/2.e9230246.chunk.js"
+  },
+  {
+    "revision": "bc382cdcc27b4173f7f0",
+    "url": "/static/js/main.6922053e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
